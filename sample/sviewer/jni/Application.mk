aeb60d3bc28fbe8ad03fd13c01ed44c1039b0e18
Wayne Wolf <wayne.ron.wu@googlemail>
2014-11-21 10:09:41 +0800

    add -gdwarf-2 compiler flag to help debugger to see local variable
    thanks to the article:
    http://stackoverflow.com/questions/12595631/debugging-with-gdb-on-a-program-with-no-optimization-but-still-there-is-no-symbo
    
    Signed-off-by: Wayne Wolf <wayne.ron.wu@googlemail>

--- sample/sviewer/jni/Application.mk
+++ sample/sviewer/jni/Application.mk
@@ -1,6 +1,6 @@
 APP_ABI                     := armeabi-v7a
 APP_PLATFORM                := android-18
 APP_STL                     := gnustl_shared
-APP_CPPFLAGS                += -frtti
+APP_CPPFLAGS                += -frtti -gdwarf-2
 NDK_TOOLCHAIN_VERSION       := 4.8
 APP_OPTIM                   := debug
